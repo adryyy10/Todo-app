d8f47bbcc7b3352635c50ff7693993072951d352
adryyy10 <adriafigueresgarcia@gmail.com>
2020-11-25 18:50:39 +0100

    Axios Update todo

--- migrations/Version20201125173440.php
+++ migrations/Version20201125173440.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201125150843 extends AbstractMigration
+final class Version20201125173440 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -37,7 +37,7 @@ class TodoController extends AbstractController
     }
 
     /**
-     * @Route("/create", name="api_todo_create")
+     * @Route("/create", name="api_todo_create", methods={"POST"})
      */
     public function create(Request $request)
     {
@@ -49,6 +49,41 @@ class TodoController extends AbstractController
 
         try {
             $this->entityManager->persist($todo);
+            $this->entityManager->flush();
+            return $this->json([
+                'todo' => $todo->toArray(),
+            ]);
+        } catch (\Throwable $th) {
+            
+        }
+    }
+
+    /**
+     * @Route("/update/{id}", name="api_todo_update", methods={"PUT"})
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $content = json_decode($request->getContent());
+
+        $todo->setName($content->name);
+
+        try {
+            $this->entityManager->flush();
+            return $this->json([
+                'todo' => $todo->toArray(),
+            ]);
+        } catch (\Throwable $th) {
+            
+        }
+    }
+
+    /**
+     * @Route("/delete/{id}", name="api_todo_delete", methods={"DELETE"})
+     */
+    public function delete(Todo $todo)
+    {
+        try {
+            $this->entityManager->remove($todo);
             $this->entityManager->flush();
             return $this->json([
                 'todo' => $todo->toArray(),
